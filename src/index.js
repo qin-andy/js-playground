ee345a5c0bbf0f71f254cead297eb9e021d4da4a
Andy <andyqin17@gmail.com>
2021-04-23 16:19:18 -0700

    Add Procfile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.static("src"));
